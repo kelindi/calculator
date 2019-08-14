ff07759f22f44171c93bb8dcbe988d3b8002ddcb
Mr.Struggle <ayushk2020@gmail.com>
2019-08-14 11:59:40 -0700

    Created Base Calculator Ui

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-function add(a,b) {
+function add(a, b) {
     return a + b;
 }
 
-function subtract(a,b){
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply(a,b) {
+function multiply(a, b) {
     return a * b;
 }
 
-function divide(a,b) {
+function divide(a, b) {
     return a / b;
+}
+
+function operatoe(opp, a, b) {
+    switch (opp) {
+        case add:
+            add(a, b);
+            break;
+        case subtract:
+            subtract(a, b);
+            break;
+        case multiply:
+            multiply(a, b);
+            break;
+        case subtract:
+            divide(a, b);
+            break;
+        default:
+            break;
+    }
 }
